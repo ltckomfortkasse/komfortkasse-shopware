37a3c70e16d61766e915a554eb1f79ba36697b43
machajdik <manuel.machajdik@gmail.com>
2016-04-26 13:58:48 +0200

    Added Developer Prefix and Icon

--- LtcKomfortkasse/Bootstrap.php
+++ LtcKomfortkasse/Bootstrap.php
@@ -1,15 +1,44 @@
 <?php
 
-class Shopware_Plugins_Backend_Komfortkasse_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+class Shopware_Plugins_Backend_LtcKomfortkasse_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
     public function getVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     public function getLabel()
     {
-        return 'Komfortkasse';
+        return 'Komfortkasse - Zahlung per Banküberweisung';
+    }
+
+    public function getDescription()
+    {
+        return 'Vollautomatische Verarbeitung von Zahlungen per Banküberweisung (Vorkasse, Rechnung, Nachnahme)';
+    }
+
+
+
+    public function getInfo()
+    {
+        return array(
+                'version' => $this->getVersion(),
+                'copyright' => 'Copyright (c) Komfortkasse',
+                'label' => $this->getLabel(),
+                'supplier' => 'Komfortkasse',
+                'description' => $this->getDescription(),
+                'support' => 'https://komfortkasse.eu/support',
+                'link' => 'https://komfortkasse.eu'
+        );
+    }
+
+    public function getCapabilities()
+    {
+        return array(
+                'install' => true,
+                // 'update' => true,
+                'enable' => true
+        );
     }
 
     public function install()
@@ -21,7 +50,7 @@ class Shopware_Plugins_Backend_Komfortkasse_Bootstrap extends Shopware_Component
         return true;
     }
 
-    
+
     public function onPostDispatchCheckout(Enlight_Event_EventArgs $arguments)
     {
         $subject = $arguments->getSubject();
@@ -32,18 +61,18 @@ class Shopware_Plugins_Backend_Komfortkasse_Bootstrap extends Shopware_Component
         $temp_id = $_SESSION['Shopware']['sessionId'];
         $id = Shopware()->Db()->fetchOne("SELECT id FROM s_order WHERE temporaryID = ?", array($temp_id));
         $site_url = 'http://'.Shopware()->System()->sCONFIG["sBASEPATH"];
-        
+
         if ( $action === 'finish'){
 
 			$query = http_build_query(array ('id' => $id,'url' => $site_url
 			));
-			
+
 			$contextData = array ('method' => 'POST','timeout' => 2,'header' => "Connection: close\r\n" . 'Content-Length: ' . strlen($query) . "\r\n",'content' => $query
 			);
-			
+
 			$context = stream_context_create(array ('http' => $contextData
 			));
-			
+
 			$result = @file_get_contents('http://api.komfortkasse.eu/api/shop/neworder.jsf', false, $context);
         }
 
