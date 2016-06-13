490f07f4ee4b69ef860f3745888b99971cf86cc0
kk-gs <gs@komfortkasse.eu>
2016-06-13 07:59:00 +0200

    Version 1.1
    Multi Shop functionality, invoice number

--- Komfortkasse/Bootstrap.php
+++ Komfortkasse/Bootstrap.php
@@ -1,52 +1,90 @@
 <?php
-
-class Shopware_Plugins_Backend_Komfortkasse_Bootstrap extends Shopware_Components_Plugin_Bootstrap
-{
-    public function getVersion()
-    {
-        return '1.0.0';
-    }
-
-    public function getLabel()
-    {
-        return 'Komfortkasse';
-    }
-
-    public function install()
-    {
-        $this->subscribeEvent(
-            'Enlight_Controller_Action_PostDispatch_Frontend_Checkout','onPostDispatchCheckout'
-        );
-
-        return true;
-    }
-
-    
-    public function onPostDispatchCheckout(Enlight_Event_EventArgs $arguments)
-    {
-        $subject = $arguments->getSubject();
-        $request  = $subject->Request();
-        $response = $subject->Response();
-        $action = $request->getActionName();
-
-        $temp_id = $_SESSION['Shopware']['sessionId'];
-        $id = Shopware()->Db()->fetchOne("SELECT id FROM s_order WHERE temporaryID = ?", array($temp_id));
-        $site_url = 'http://'.Shopware()->System()->sCONFIG["sBASEPATH"];
-        
-        if ( $action === 'finish'){
-
-			$query = http_build_query(array ('id' => $id,'url' => $site_url
-			));
+class Shopware_Plugins_Backend_Komfortkasse_Bootstrap extends Shopware_Components_Plugin_Bootstrap {
+	public function getVersion() {
+	}
+	public function getLabel() {
+		return 'Komfortkasse';
+	}
+	public function install() {
+		Shopware ()->Loader ()->registerNamespace ( 'Shopware_Components_Komfortkasse', dirname ( __FILE__ ) . '/Components/Komfortkasse/' );
+		$this->subscribeEvent ( 'Enlight_Controller_Action_PostDispatch_Frontend_Checkout', 'onPostDispatchCheckout' );
+		$this->subscribeEvent ( 'Shopware_Components_Document::assignValues::after', 'afterCreatingDocument' );
+		
+		$form = $this->Form ();
+		$parent = $this->Forms ()->findOneBy ( array (
+				'name' => 'Frontend' 
+		) );
+		$form->setParent ( $parent );
+		$form->setElement ( 'checkbox', 'active', array (
+				'label' => 'Plugin aktivieren',
+				'value' => true,
+				'scope' => Shopware\Models\Config\Element::SCOPE_SHOP 
+		) );
+		
+		$this->addFormTranslations ( array (
+				'en_GB' => array (
+						'plugin_form' => array (
+								'label' => 'activate plugin' 
+						) 
+				) 
+		) );
+		
+		return true;
+	}
+	public function onPostDispatchCheckout(Enlight_Event_EventArgs $arguments) {
+		$subject = $arguments->getSubject ();
+		$request = $subject->Request ();
+		$response = $subject->Response ();
+		$action = $request->getActionName ();
+		
+		$temp_id = $_SESSION ['Shopware'] ['sessionId'];
+		$id = Shopware ()->Db ()->fetchOne ( "SELECT id FROM s_order WHERE temporaryID = ?", array (
+				$temp_id 
+		) );
+		
+		if ($action === 'finish') {
 			
-			$contextData = array ('method' => 'POST','timeout' => 2,'header' => "Connection: close\r\n" . 'Content-Length: ' . strlen($query) . "\r\n",'content' => $query
+			$query = http_build_query ( array (
+					'id' => $id,
+					'url' => $site_url 
+			) );
+			
+			$contextData = array (
+					'method' => 'POST',
+					'timeout' => 2,
+					'header' => "Connection: close\r\n" . 'Content-Length: ' . strlen ( $query ) . "\r\n",
+					'content' => $query 
 			);
 			
-			$context = stream_context_create(array ('http' => $contextData
-			));
+			$context = stream_context_create ( array (
+					'http' => $contextData 
+			) );
 			
-			$result = @file_get_contents('http://api.komfortkasse.eu/api/shop/neworder.jsf', false, $context);
-        }
-
-    }
-
+			$result = @file_get_contents ( 'http://api.komfortkasse.eu/api/shop/neworder.jsf', false, $context );
+		}
+	}
+	public function afterCreatingDocument(Enlight_Hook_HookArgs $arguments) {
+		$document = $arguments->getSubject ();
+		
+		$rid = Shopware ()->Db ()->fetchOne ( "SELECT docID FROM s_order_documents WHERE orderID = ?", $document->_order->shipping ['orderID'] . " AND type = ?", 1 );
+			$query = http_build_query ( array (
+					'id' => $document->_order->shipping ['orderID'],
+					'url' => $site_url,
+					'invoice_number' => $rid 
+			) );
+			
+			$contextData = array (
+					'method' => 'POST',
+					'timeout' => 2,
+					'header' => "Connection: close\r\n" . 'Content-Length: ' . strlen ( $query ) . "\r\n",
+					'content' => $query 
+			);
+			
+			$context = stream_context_create ( array (
+					'http' => $contextData 
+			) );
+			
+			$result = @file_get_contents ( 'http://api.komfortkasse.eu/api/shop/invoice.jsf', false, $context );
+		}
+	}
 }
